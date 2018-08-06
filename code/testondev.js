00342386819d81a0ff2a9db1afe04cf2f5c50623
tangzheng <tangzheng1900@163.com>
2018-08-06 14:53:24 +0800

    dev的分支添加新代码

--- code/testondev.js
+++ code/testondev.js
@@ -4,3 +4,32 @@
  * @Update: 06/08/2018
  * @Desc:
  */
+
+class Parent{
+    constructor(Props){
+        this.siteId = '100000001';
+        this.name = 'Parent'
+    }
+
+    render(){
+        let x=10000;
+        console.log(x);
+        console.log(this);
+    }
+}
+
+
+class Child extends Parent{
+    constructor(Props){
+        super(Props);
+        this.name = 'child';
+        this.prop = Props;
+    }
+
+}
+
+var p= new Parent();
+p.render();
+
+var c = new Child({'age': 28});
+c.render();
